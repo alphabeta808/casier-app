c3d126c6a7c7c38754c452658902b21685646a5f
alvan77 <alfanmaulana777@gmail.com>
2023-01-28 17:49:21 +0700

    casier app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+// import Navigation from "./components/Navigation";
+import './index.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="dashboard text-white flex h-screen">
+      <div className="aside side-bar w-1/5 bg-orange-500">
+        <div className="border-b-2 border-slate-700 pb-5">
+          <h1 className='px-5 pt-5 text-xl'>Dashboard</h1>
+        </div>
+        <ul className="menu flex flex-col">
+          <li><a href="#">Barang</a></li>
+          <li><a href="#">Penjualan</a></li>
+          <li><a href="#">Pembelian</a></li>
+          <li><a href="#">Supplier</a></li>
+          <li><a href="#">Laporan Penjualan</a></li>
+          <li><a href="#">Laporan Pembelian</a></li>
+        </ul>
+      </div>
+      <div className="aside w-4/5 bg-green-500">
+        <div className="header flex justify-between px-5 pt-5 border-b-2 border-slate-700 pb-5">
+          <h1>Toko Alfan</h1>
+          <nav>
+            <ul className="flex">
+              <li className='px-5'><a href="#">Users</a></li>
+              <li><a href="#">Settings</a></li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+
     </div>
   );
 }
---
+++ src/components/Navigation.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navigation = () => {
+    return (
+        <div>
+            <header className="bg-black w-full h-1">
+                Navigation
+            </header>
+        </div>
+    )
+}
+
+export default Navigation
